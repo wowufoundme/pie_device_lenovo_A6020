dbc3c65468de94be933643e4901dfd11a2b22dab
Yash-Garg <ben10.yashgarg@gmail.com>
2018-09-21 21:41:30 +0200

    A6020: cleanup: Move stuff from full_A6020.mk
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,5 +15,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_A6020.mk \
     $(LOCAL_DIR)/lineage_A6020.mk
--- lineage_A6020.mk
+++ lineage_A6020.mk
@@ -12,7 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/lenovo/A6020/full_A6020.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from A6020 device
+$(call inherit-product, device/lenovo/A6020/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product-if-exists, vendor/lineage/config/common_full_phone.mk)
@@ -25,6 +31,9 @@ TARGET_BOOTANIMATION_HALF_RES := true
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A6020
 PRODUCT_NAME := lineage_A6020
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Vibe K5
+PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
 # Unofficial build ID
